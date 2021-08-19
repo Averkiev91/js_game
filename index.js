af022858adf0ddb1ddce46f8294325528aebd312
Averkiev91 <amd3500m@gmail.com>
2021-08-19 21:38:39 +0300

    endGame

--- index.js
+++ index.js
@@ -1,19 +1,78 @@
 let $start = document.querySelector('#start')
 let $game = document.querySelector('#game')
+let $time = document.querySelector('#time')
+let $result = document.querySelector('#result')
+let $timeHeader = document.querySelector('#time-header')
+let $resultHeader = document.querySelector('#result-header')
+let $gameTime = document.querySelector('#game-time')
+let isGameStarted = false
 let score = 0
+
+let colors = ['red', 'blue', 'green', 'yellow', 'orange']
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
+$gameTime.addEventListener('input', setGameTime)
+
+function show($el){
+    $el.classList.remove('hide')
+}
+
+function hide($el) {
+    $el.classList.add('hide')
+}
+
 
 function startGame() {
+    score = 0
+    isGameStarted = true
     game.style.backgroundColor = '#fff'
-    $start.classList.add('hide')
+    hide($start)
+    setGameTime()
+    $gameTime.setAttribute('disabled', 'true')
+
+    let interval = setInterval(function() {
+        let time = parseFloat($time.textContent)
+
+        if (time <= 0) {
+            clearInterval(interval)
+            endGame()
+        } else {
+            $time.textContent = (time - 0.1).toFixed(1)
+        }
+    }, 100)
 
     renderBox()
 }
 
+function setGameScore() {
+    $result.textContent = score.toString()
+}
+
+function setGameTime() {
+    let time = +$gameTime.value
+    $time.textContent = time.toFixed(1)
+    show($timeHeader)
+    hide($resultHeader)
+}
+
+function endGame() {
+    isGameStarted = false
+    setGameScore()
+    $gameTime.removeAttribute('disabled')
+    show($start)
+    $game.innerHTML = ''
+    $game.style.backgroundColor = '#ccc'
+    hide($timeHeader)
+    show($resultHeader)
+}
+
 function handleBoxClick() {
-    if (event.target.dataset) {
+    if (!isGameStarted) {
+        return
+    }
+
+    if (event.target.dataset.box) {
         score++
         renderBox()
     }
@@ -29,7 +88,7 @@ function renderBox() {
 
     box.style.height = box.style.width = boxSize + 'px'
     box.style.position = 'absolute'
-    box.style.backgroundColor = 'black'
+    box.style.backgroundColor = colors[getRandom(0, colors.length)]
     box.style.top = getRandom(0, maxTop) + 'px'
     box.style.left = getRandom(0, maxLeft) + 'px'
     box.style.cursor = 'pointer'
